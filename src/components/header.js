6c488a6cf1939cb7e518b9744ba2e6c6b140e86f
Peter Arnold <peter@deepsixdesign.com>
2019-12-21 12:51:05 -0500

    working hooks

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import Link from "../components/Link"
+import Link from "../components/link"
 import PropTypes from "prop-types"
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
--- src/components/predict.js
+++ src/components/predict.js
@@ -1,101 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import "@tensorflow/tfjs";
-import "./styles.css";
+import Container from "@material-ui/core/Container"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
 
-export default class Predict extends React.Component {
-  videoRef = React.createRef();
-  canvasRef = React.createRef();
+import { useObjects } from "use-tensorflow";
 
-  componentDidMount() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      const webCamPromise = navigator.mediaDevices
-        .getUserMedia({
-          audio: false,
-          video: {
-            facingMode: "environment"
-          }
-        })
-        .then(stream => {
-          window.stream = stream;
-          this.videoRef.current.srcObject = stream;
-          return new Promise((resolve, reject) => {
-            this.videoRef.current.onloadedmetadata = () => {
-              resolve();
-            };
-          });
-        });
-      const modelPromise = cocoSsd.load();
+import pic from "../images/room.jpg"
 
-      Promise.all([modelPromise, webCamPromise])
-        .then(values => {
-          this.detectFrame(this.videoRef.current, values[0]);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }
+const Predict = () => {
 
-  detectFrame = (video, model) => {
-    model.detect(video).then(predictions => {
-      this.renderPredictions(predictions);
-      requestAnimationFrame(() => {
-        this.detectFrame(video, model);
-      });
-    });
-  };
+  const ref = useRef(null);
+  const objects = useObjects(ref);
 
-  renderPredictions = predictions => {
-    const ctx = this.canvasRef.current.getContext("2d");
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // Font options.
-    const font = "16px";
-    ctx.font = font;
-    ctx.textBaseline = "top";
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      const width = prediction.bbox[2];
-      const height = prediction.bbox[3];
-      // Draw the bounding box.
-      ctx.strokeStyle = "#FF1654";
-      ctx.lineWidth = 4;
-      ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
-      ctx.fillStyle = "#FF1654";
-      const textWidth = ctx.measureText(prediction.class).width;
-      const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-    });
-
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#fff";
-      ctx.fillText(prediction.class, x, y);
-    });
-  };
-
-  render() {
-    return (
-      <React.Fragment><video
-      className="size"
-      autoPlay
-      playsInline
-      muted
-      ref={this.videoRef}
-      width="600"
-      height="500"
-    />
-    <canvas
-      className="size"
-      ref={this.canvasRef}
-      width="600"
-      height="500"
-    /></React.Fragment>
-    );
-  }
+  return(
+    <Container maxWidth="sm">
+      <Box my={4}>
+      <img ref={ref} src={pic} />
+      {objects ? objects.map(({ left, top, width, height, label, score }) => (
+        <Typography variant="h4" component="h4" gutterBottom align="center">
+        {left}-{top}-{width}-{height}-{label}-{score}
+      </Typography>
+      )) : 'Loading...'}
+        
+      </Box>
+    </Container>
+  )
 }
+export default Predict
