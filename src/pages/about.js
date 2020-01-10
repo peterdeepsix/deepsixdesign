3c4f58f426d1503e8517a0de943dce4170896542
Peter Arnold <peter@deepsixdesign.com>
2020-01-10 03:06:54 +0000

    sick brid

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useFirebase } from 'gatsby-plugin-firebase';
 
+import DefaultLayout from '../layouts/defaultLayout';
+import SEO from '../components/seo';
+
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-
-import DefaultLayout from '../layouts/defaultLayout';
-import SEO from '../components/seo';
 
 const About = () => {
   useFirebase(firebase => {
--- src/pages/media.js
+++ src/pages/media.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useFirebase } from 'gatsby-plugin-firebase';
 
 import DefaultLayout from '../layouts/defaultLayout';
 import SEO from '../components/seo';
 
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const Media = () => {
+  useFirebase(firebase => {
+    firebase.analytics().logEvent('visited_media');
+  }, []);
   return (
     <DefaultLayout>
       <SEO title="Media" />
-      <Container maxWidth="md">
-        <Box my={4}>Media</Box>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="body1">Media</Typography>
+        </Box>
       </Container>
     </DefaultLayout>
   );
