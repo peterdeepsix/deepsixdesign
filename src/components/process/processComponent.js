8d8326f86c741544596bba51090f01459d95277f
Peter Arnold <peter@deepsixdesign.com>
2020-01-16 16:28:38 +0000

    nice timeline :D lovely

--- src/components/process/processComponent.js
+++ src/components/process/processComponent.js
@@ -1,4 +1,42 @@
 import React from 'react';
-export default () => {
-  return <div>Hello blog post</div>;
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Card from '@material-ui/core/Card';
+
+const styles = ({ breakpoints, transitions }) => ({
+  root: {
+    padding: 16,
+    transition: transitions.create(),
+    [breakpoints.up('sm')]: {
+      padding: 24,
+      maxWidth: 700,
+      margin: 'auto',
+    },
+    [breakpoints.up('md')]: {
+      maxWidth: 960,
+    },
+  },
+});
+
+const ProcessComponent = ({ data, classes }) => {
+  const node = data.allProcesses.edges[0].node;
+  return (
+    <Card className={classes.card} elevation={0}>
+      <div className={classes.root}>
+        <Typography variant={'overline'}>Details</Typography>
+        <Typography variant={'h4'} gutterBottom>
+          {node.title}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Step {node.step}
+        </Typography>
+        <Typography variant="body1" indent={'small'}>
+          {node.definition}
+        </Typography>
+      </div>
+    </Card>
+  );
 };
+
+export default withStyles(styles)(ProcessComponent);
--- src/templates/process.js
+++ src/templates/process.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+
 import AppLayout from '../components/layouts/appLayout';
 import StoreLayout from '../components/layouts/storeLayout';
 import SEO from '../components/seo';
@@ -13,12 +15,29 @@ const ProcessComponent = Loadable(
   },
 );
 
-export default () => {
+export const query = graphql`
+  query($slug: String!) {
+    allProcesses(filter: { slug: { eq: $slug } }) {
+      edges {
+        node {
+          id
+          slug
+          title
+          step
+          definition
+          synonyms
+        }
+      }
+    }
+  }
+`;
+
+export default ({ data }) => {
   return (
     <AppLayout>
       <StoreLayout>
         <SEO title="Timeline" />
-        <ProcessComponent />
+        <ProcessComponent data={data} />
       </StoreLayout>
     </AppLayout>
   );
