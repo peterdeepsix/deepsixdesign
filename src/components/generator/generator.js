5893cafc8755ba724ba2257bd64632cb53d6635b
Peter Arnold <peter@deepsixdesign.com>
2020-01-19 21:57:41 +0000

    charge hard

--- src/components/generator/generator.js
+++ src/components/generator/generator.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react';
-import usePersistentState from '../../hooks/usePeristentState';
 import usePersistentCanvas from '../../hooks/usePersistentCanvas';
+import Button from '@material-ui/core/Button';
 
-const HOOK_SVG =
-  'm129.03125 63.3125c0-34.914062-28.941406-63.3125-64.519531-63.3125-35.574219 0-64.511719 28.398438-64.511719 63.3125 0 29.488281 20.671875 54.246094 48.511719 61.261719v162.898437c0 53.222656 44.222656 96.527344 98.585937 96.527344h10.316406c54.363282 0 98.585938-43.304688 98.585938-96.527344v-95.640625c0-7.070312-4.640625-13.304687-11.414062-15.328125-6.769532-2.015625-14.082032.625-17.960938 6.535156l-42.328125 64.425782c-4.847656 7.390625-2.800781 17.3125 4.582031 22.167968 7.386719 4.832032 17.304688 2.792969 22.160156-4.585937l12.960938-19.71875v42.144531c0 35.582032-29.863281 64.527344-66.585938 64.527344h-10.316406c-36.714844 0-66.585937-28.945312-66.585937-64.527344v-162.898437c27.847656-7.015625 48.519531-31.773438 48.519531-61.261719zm-97.03125 0c0-17.265625 14.585938-31.3125 32.511719-31.3125 17.929687 0 32.511719 14.046875 32.511719 31.3125 0 17.261719-14.582032 31.3125-32.511719 31.3125-17.925781 0-32.511719-14.050781-32.511719-31.3125zm0 0';
-const HOOK_PATH = new Path2D(HOOK_SVG);
-const SCALE = 0.3;
-const OFFSET = 80;
-
-const size = window.innerWidth;
-
+var white = '#F2F5F1';
+var colors = ['#D40920', '#1356A2', '#F7D842'];
+const size = 300;
+const step = size / 6;
 const squares = [
   {
     x: 0,
@@ -20,6 +16,14 @@ const squares = [
 ];
 
 function draw(ctx, location) {
+  for (var i = 0; i < size; i += step) {
+    splitSquaresWith({ y: i });
+    splitSquaresWith({ x: i });
+  }
+  for (var i = 0; i < colors.length; i++) {
+    squares[Math.floor(Math.random() * squares.length)].color =
+      colors[i];
+  }
   for (var i = 0; i < squares.length; i++) {
     ctx.beginPath();
     ctx.rect(
@@ -28,18 +32,80 @@ function draw(ctx, location) {
       squares[i].width,
       squares[i].height,
     );
+    if (squares[i].color) {
+      ctx.fillStyle = squares[i].color;
+    } else {
+      ctx.fillStyle = white;
+    }
+    ctx.fill();
     ctx.stroke();
   }
 }
 
-draw();
+function splitSquaresWith(coordinates) {
+  const { x, y } = coordinates;
+
+  for (var i = squares.length - 1; i >= 0; i--) {
+    const square = squares[i];
+
+    if (x && x > square.x && x < square.x + square.width) {
+      if (Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnX(square, x);
+      }
+    }
+
+    if (y && y > square.y && y < square.y + square.height) {
+      if (Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnY(square, y);
+      }
+    }
+  }
+}
+
+function splitOnX(square, splitAt) {
+  var squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width - (square.width - splitAt + square.x),
+    height: square.height,
+  };
+
+  var squareB = {
+    x: splitAt,
+    y: square.y,
+    width: square.width - splitAt + square.x,
+    height: square.height,
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
+}
+
+function splitOnY(square, splitAt) {
+  var squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width,
+    height: square.height - (square.height - splitAt + square.y),
+  };
+
+  var squareB = {
+    x: square.x,
+    y: splitAt,
+    width: square.width,
+    height: square.height - splitAt + square.y,
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
+}
 
 const Generator = () => {
   const [locations, setLocations, canvasRef] = usePersistentCanvas(
     draw,
   );
-
-  const context = canvasRef.getContext('2d');
 
   function handleCanvasClick(e) {
     const newLocation = { x: e.clientX, y: e.clientY };
@@ -52,11 +118,16 @@ const Generator = () => {
     setLocations(locations.slice(0, -1));
   }
 
+  React.useEffect(() => {
+    draw();
+  });
+
   return (
     <>
       <div className="controls">
-        <button onClick={handleClear}>Clear</button>
-        <button onClick={handleUndo}>Undo</button>
+        <Button onClick={handleCanvasClick}>Draw</Button>
+        <Button onClick={handleClear}>Clear</Button>
+        <Button onClick={handleUndo}>Undo</Button>
       </div>
       <canvas
         ref={canvasRef}
