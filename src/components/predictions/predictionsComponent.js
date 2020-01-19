19af8ee05c12af16d3f05c052c9f36ce7ef94b6a
Peter Arnold <peter@deepsixdesign.com>
2020-01-19 02:13:25 +0000

    asd

--- src/components/predictions/predictionsComponent.js
+++ src/components/predictions/predictionsComponent.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
 import PredictionsTable from './predictionsTable';
-import PredictionsDialog from './predictionsDialog';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 const PredictionsComponent = () => {
+  const classes = useStyles();
   return (
-    <Container>
-      {/* <Box>
-        <PredictionsDialog />
-      </Box> */}
-      <Box>
-        <PredictionsTable />
-      </Box>
-    </Container>
+    <div className={classes.container}>
+      <Container>
+        <Box>
+          <PredictionsTable />
+        </Box>
+      </Container>
+    </div>
   );
 };
 
