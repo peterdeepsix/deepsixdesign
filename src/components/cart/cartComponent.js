8a3804035130c612faf126b7a6c00aeca087bff9
Peter Arnold <peter@deepsixdesign.com>
2020-02-01 20:24:56 -0500

    working stripe

--- src/components/cart/cartComponent.js
+++ src/components/cart/cartComponent.js
@@ -2,43 +2,22 @@ import React, { useContext } from 'react';
 import { CartContext } from './cartProvider';
 import Checkout from './checkoutComponent';
 import CartItem from './cartItem';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const CartComponent = () => {
   const { cart, count, mode, toggle } = useContext(CartContext);
   return (
     <>
-      <button
-        onClick={() => toggle()}
-        style={{
-          position: 'fixed',
-          right: '2rem',
-          top: '2rem',
-          zIndex: '2',
-        }}
-      >
+      <Button variant="outlined" onClick={() => toggle()}>
         {mode ? '→' : 'cart'}
-      </button>
-      <div
-        style={{
-          display: mode ? 'initial' : 'none',
-          position: 'fixed',
-          right: 0,
-          top: 0,
-          height: '100vh',
-          padding: '2rem',
-          backgroundColor: 'white',
-          maxWidth: 400,
-          width: '100%',
-          zIndex: 1,
-        }}
-      >
-        <h1>cart</h1>
-        {count === 0 && <span>No items in cart.</span>}
-        {cart.map(([sku, quantity]) => (
-          <CartItem key={sku.id} sku={sku} quantity={quantity} />
-        ))}
-        <Checkout />
-      </div>
+      </Button>
+      <Typography>Shopping Cart</Typography>
+      {count === 0 && <span>No items in cart.</span>}
+      {cart.map(([sku, quantity]) => (
+        <CartItem key={sku.id} sku={sku} quantity={quantity} />
+      ))}
+      <Checkout />
     </>
   );
 };
