c7e0028d70d26b1d593b27d886cd4670b9453304
Peter Arnold <peter@deepsixdesign.com>
2020-02-19 09:32:28 -0500

    lovely

--- src/components/mainLayout/components/navBar.js
+++ src/components/mainLayout/components/navBar.js
@@ -1,64 +1,141 @@
 import React, { useState } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
-import NavBreadCrumbs from './navBreadCrumbs';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ChangeHistoryOutlinedIcon from '@material-ui/icons/ChangeHistoryOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
-    backgroundColor: '#fff',
+    backgroundColor: theme.palette.common.white,
   },
   toolbar: {},
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
 }));
 
-function HideOnScroll(props) {
+function Scroll(props) {
   const { children } = props;
-  const trigger = useScrollTrigger({});
+
+  const trigger1 = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  const trigger2 = useScrollTrigger({});
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
+    <Slide appear={false} direction="down" in={!trigger2}>
       {React.cloneElement(children, {
-        elevation: trigger ? 4 : 0,
+        elevation: trigger1 ? 4 : 0,
       })}
     </Slide>
   );
 }
 
-function ElevationScroll(props) {
-  const { children } = props;
-
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-}
-
 const NavBar = props => {
   const classes = useStyles();
-  const [value, setValue] = useState(0);
+
   return (
     <>
-      <ElevationScroll {...props}>
-        <AppBar color="inherit" className={classes.appBar}>
-          <HideOnScroll {...props}>
-            <Toolbar className={classes.toolbar}>
-              <Typography variant="h6">Deep Six Design</Typography>
-            </Toolbar>
-          </HideOnScroll>
-
+      <Scroll {...props}>
+        <AppBar
+          positon="sticky"
+          color="inherit"
+          className={classes.appBar}
+        >
           <Toolbar className={classes.toolbar}>
-            <NavBreadCrumbs />
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <IconButton color="inherit" aria-label="open drawer">
+              <ChangeHistoryOutlinedIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Deep Six Design
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+
+            <IconButton color="inherit">
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <AccountCircleOutlinedIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
-      </ElevationScroll>
+      </Scroll>
+      <Toolbar className={classes.toolbar}></Toolbar>
     </>
   );
 };
